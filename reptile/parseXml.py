85b6157f79cd2651d16191d8bd323a5f6ae3e890
wenka <wkwenka@gmail.com>
2020-06-11 09:26:44 +0800

    分页获取CSDN博主的全部文章

--- reptile/parseXml.py
+++ reptile/parseXml.py
@@ -4,8 +4,33 @@ import execjs
 from reptile import httpUtil
 
 article_xpath = '//div[@class="article-list"]/div[@data-articleid]'
-
 path_xpath = '//script'
+
+blog_name = 'menglinjie'
+
+
+class PageInfo:
+
+    def __init__(self, total, page_size=40):
+        self.page_no = 0
+        self.total = total
+        self.page_size = page_size
+        # 计算总页数
+        if total % page_size > 0:
+            self.total_page = total // page_size + 1
+        else:
+            self.total_page = total // page_size
+
+    def has_next(self):
+        return self.page_no < self.total_page
+
+    def next_page(self):
+        self.page_no = self.page_no + 1
+        return self.page_no
+
+    def __repr__(self):
+        return "<PageInfo(current_page:%s, total_page:%s, total:%s, page_size:%s)>" % (
+            self.page_no + 1, self.total_page, self.total, self.page_size)
 
 
 class Article:
@@ -28,7 +53,22 @@ class Article:
             self.id, self.title, self.create_time, self.read_num, self.comment_num, self.url)
 
 
+def get_page_url(current_page):
+    """
+    获取分页Url
+    :param current_page:
+    :return:
+    """
+    page_url = 'https://blog.csdn.net/%s/article/list/' % (blog_name)
+    return page_url + str(current_page)
+
+
 def get_page_info(blog_url):
+    """
+    获取分页信息
+    :param blog_url:
+    :return:
+    """
     response_html = httpUtil.Request(url=blog_url).get_response()
     html = etree.HTML(response_html)
     path_html = html.xpath(path_xpath)
@@ -37,7 +77,6 @@ def get_page_info(blog_url):
         return
     for script in path_html:
         if script.text is not None and 'listTotal' in script.text:
-            print(script.text)
             execjs.compile(script.text)
             get_total_script = '''
             function getTotalCount(){
@@ -46,22 +85,25 @@ def get_page_info(blog_url):
             function getCurrentPage(){
                 return currentPage;
             }
+            function getPageSize(){
+                return pageSize ;
+            }
             '''
             execjs_compile = execjs.compile(script.text + get_total_script)
             list_total = execjs_compile.call('getTotalCount')
-            current_page = execjs_compile.call('getCurrentPage')
+            page_size = execjs_compile.call('getPageSize')
             print('总文章数量：', list_total)
-            print('当前页码：', current_page)
-    pass
+            print('分页大小：', page_size)
+            return PageInfo(page_size=page_size, total=list_total)
 
 
-def get_data(blog_url):
+def get_data(page_blog_url):
     """
      通过 CSDN 播客主页地址 获取该博主的博客内容
-    :param blog_url:  CSDN 播客主页地址
+    :param page_blog_url:  CSDN 播客分页地址
     :return: 文章列表
     """
-    response_html = httpUtil.Request(url=blog_url).get_response()
+    response_html = httpUtil.Request(url=page_blog_url).get_response()
     html = etree.HTML(response_html)
     articles = html.xpath(article_xpath)
     # Article 文章对象列表
@@ -90,10 +132,21 @@ def get_data(blog_url):
         article.content = content
         article_list.append(article)
 
-    print('总共获取文章【%s】篇。' % (len(article_list)))
+    print('%s 总共获取文章【%s】篇。' % (page_blog_url, len(article_list)))
     return article_list
 
 
 if __name__ == '__main__':
-    url = 'https://blog.csdn.net/menglinjie'
-    get_page_info(url)
+    url = 'https://blog.csdn.net/' + blog_name
+    page_info = get_page_info(url)
+    articles = []
+    while page_info.has_next():
+        page = page_info.next_page()
+        page_url = get_page_url(page)
+        article_list = get_data(page_blog_url=page_url)
+        articles.extend(article_list)
+
+    print('总共获取文章【%s】篇。' % len(articles))
+
+    for a in articles:
+        print(a)
